42e4542d4158b9b268090c0039aabfcbfe5a278d
DavidBarker05 <davidbarker9611@gmail.com>
2025-10-19 15:21:41 +0200

    adjusted fire rate

--- Source/FirstPersonShooter/Weapons/PistolWeapon.cpp
+++ Source/FirstPersonShooter/Weapons/PistolWeapon.cpp
@@ -2,7 +2,7 @@
 #include "Weapons/WeaponShootComponent.h"
 
 APistolWeapon::APistolWeapon() {
-	WeaponShootComponent->TimeBetweenShots = 0.5f;
+	WeaponShootComponent->TimeBetweenShots = 0.1f;
 	WeaponShootComponent->GunDamage = 3;
 	WeaponShootComponent->BulletSpeed = 3000.0f;
 	WeaponShootComponent->VerticalRecoil = 1.0f;
